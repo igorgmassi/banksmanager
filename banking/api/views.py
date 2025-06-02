7655e406ab637c5124aee34470facfef9897f079
igorg.massi <igorg.massi@gmail.com>
2025-06-01 23:15:27 -0300

    Ajustando as views para ela ter filtro

--- banking/api/views.py
+++ banking/api/views.py
@@ -1,23 +1,34 @@
 from rest_framework import viewsets
 from banking.models import Account, Transaction, Loan, Card, User
+from django_filters.rest_framework import DjangoFilterBackend
 from banking.api.serializers import UserSerializer, AccountSerializer, TransactionSerializer, LoanSerializer, CardSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = '__all__'  
 
 class AccountViewSet(viewsets.ModelViewSet):
     queryset = Account.objects.all()
     serializer_class = AccountSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = '__all__'  
 
 class TransactionViewSet(viewsets.ModelViewSet):
     queryset = Transaction.objects.all()
     serializer_class = TransactionSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = '__all__'  
 
 class LoanViewSet(viewsets.ModelViewSet):
     queryset = Loan.objects.all()
     serializer_class = LoanSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = '__all__'  
 
 class CardViewSet(viewsets.ModelViewSet):
     queryset = Card.objects.all()
     serializer_class = CardSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = '__all__'  
